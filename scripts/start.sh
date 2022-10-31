14e16b696cf1bc6e423292134e64f8221c3c9f8d
jigga.e10@gmail.com <jigga.e10@gmail.com>
2022-10-31 08:39:19 +0100

    added ci/cd files as well as devops scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #! bin/bash
 
+pm2 stop ./scripts/serve.sh && pm2 start ./scripts/serve.sh
+
 touch /var/spool/cron/jigga_e10# /usr/bin/crontab /var/spool/cron/jigga_e10
 
 echo "0 * * * * /home/jigga_e10/kashmore_node/scripts/restart.sh" >> /var/spool/cron/jigga_e10
