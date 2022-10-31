979352a6ceea899eb057196468c8203d818bd2a5
jigga.e10@gmail.com <jigga.e10@gmail.com>
2022-10-31 15:09:38 +0100

    added ci/cd files as well as devops scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,16 @@
 #! bin/bash
 
+servePM = sudo pm2 stop serve
+
+if [ $servePM -ne 0 ];
+then
+
+else
+
+fi
+
+sudo npm uninstall -g pm2 && sudo -u jigga_e10 pm2 start scripts/serve.sh
+
 sudo touch /var/spool/cron/jigga_e10# /usr/bin/crontab /var/spool/cron/jigga_e10
 
 sudo echo "0 * * * * /home/jigga_e10/kashmore_node/scripts/restart.sh" >> /var/spool/cron/jigga_e10
