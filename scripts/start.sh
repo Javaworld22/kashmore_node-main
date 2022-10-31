56793a064c694ffabf6d01cd4495572d606edcbd
jigga.e10@gmail.com <jigga.e10@gmail.com>
2022-10-31 17:42:24 +0100

    added ci/cd files as well as devops scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #! bin/bash
 
-sudo pm2 stop serve || echo "Serve script not running ..."
+sudo -u jigga_e10 pm2 stop serve || echo "Serve script not running ..."
 
-sudo -u jigga_e10 pm2 start scripts/serve.sh
+sudo -u jigga_e10 pm2 start scripts/serve.sh --name serve
 
 sudo touch /var/spool/cron/jigga_e10# /usr/bin/crontab /var/spool/cron/jigga_e10
 
